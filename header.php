4ceb4289dd70dc852a87be098ad33b571505b275
Marton Reka <rmarton910@gmail.com>
2019-05-07 23:00:13 +0300

    moved css file to new css folder

--- header.php
+++ header.php
@@ -4,12 +4,11 @@
 <head>
 		<meta charset="UTF-8">
 		<title>TravelTheme</title>  	
-        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">     
-
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 		<?php wp_head(); ?>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-		<link href="<?php echo get_template_directory_uri(); ?>/reset.css" rel="stylesheet">		
-		<link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
+		<link href="<?php echo get_template_directory_uri(); ?>/css/reset.css" rel="stylesheet">		
+		<link href="<?php echo get_template_directory_uri(); ?>/css/style.css" rel="stylesheet">
 		<link href="<?php echo get_template_directory_uri(); ?>/css/header.css" rel="stylesheet">
 		<link href="<?php echo get_template_directory_uri(); ?>/css/footer.css" rel="stylesheet">
 </head>
@@ -20,15 +19,14 @@
 			<div class="header">
 				<div id="title">	
       				<h1>Travel in Sic village</h1>
-                  <img src="https://www.123rf.com/photo_116780938_wooden-footbridge-crossing-the-reed-reserve-from-sic-village-near-cluj-napoca-romania.html" alt="Szek" width="500" height="333">  
-      			</div>
+              <img src="https://www.cazaretransilvania.ro/Menu/Imagini/Sic-szek3.jpg" alt="Webpage">  
+      	</div>
       		<nav>
       			<ul>
     				<li><a href="index.html">Home</a></li>
     				<li><a href="about.html">Actual</a></li>
     				<li><a href="contact.html">Contact</a></li>
-    				<li><a href="search.html">⌕</a></li>
-    			</ul>
+    			 </ul>
   	 		 <nav>
   			</div>
   		</div>
